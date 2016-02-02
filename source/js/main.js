4b9985667a43f492156ba959b04624bcf3da8edc
Sam Smith <sam@mintcanary.com>
2016-02-02 17:06:49 +0000

    add members

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-    smoothScroll.init({
-        updateURL: false,
-    });
 
-    $('a[rel="external"]')
-        .click( function() {
-        window.open( $(this).attr('href') );
-        return false;
-    });
+  //grid
+  var $container = $('.members .wrapper');
+  // init
+  $container.packery({
+    itemSelector: '.member',
+    gutter: 0,
+   "columnWidth": 1,
+   "rowHeight": 1
+  });
+
+
+  //smooth scroll
+  smoothScroll.init({
+      updateURL: false,
+  });
+
+
+  //external links
+  $('a[rel="external"]')
+      .click( function() {
+      window.open( $(this).attr('href') );
+      return false;
+  });
 });
